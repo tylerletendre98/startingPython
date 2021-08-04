ec89e90bd4322990912dbb9e2e69c8be50dbb43c
T U <tylerletendre98@gmail.com>
2021-08-04 11:13:31 -0400

    added in tabbing and going over variables

---
+++ escapeChar.py
@@ -0,0 +1,7 @@
+# spliting strings over lines
+splitString = "This string has been\nsplit over\nlines"
+print(splitString)
+
+# tabbing lines
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
--- strings.py
+++ strings.py
@@ -1,5 +1,15 @@
-name = input("Please enter your name ")
-greeting = "hello " + name
+name = "Tyler"
+greeting = "Hello " + name
 
 # printing using a variable and an input function
 print(greeting)
+
+age = 24
+print(age)
+
+# Checking for the type of our variable
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(type(age))
