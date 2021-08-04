63822288a23fe2d740d116d217a0983472c885da
T U <tylerletendre98@gmail.com>
2021-08-04 11:32:14 -0400

    going over different operators and then going over their data types

--- numbers.py
+++ numbers.py
@@ -5,3 +5,20 @@ print(type(number))
 # this is an int type number
 anotherNumber = 4
 print(type(anotherNumber))
+
+a = 12
+b = 3
+
+
+print(a + b)  # addition
+print(a - b)  # subtraction
+print(a * b)  # multiply
+print(a / b)  # dividing
+print(a // b)  # dividing rounding down
+print(a % b)  # remainder after the division
+
+print()
+
+# cant use a float in this
+for i in range(1, 6):
+    print(i)
