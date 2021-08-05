c68d28abe6343546ed6f9b04bf6051bade39ea1f
T U <tylerletendre98@gmail.com>
2021-08-05 18:10:42 -0400

    using more negative indexing

---
+++ sliceBack.py
@@ -0,0 +1,17 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+# prints out the string backwards
+backwards = letters[25::-1]
+print(backwards)
+
+# prints qpo
+qpo = letters[16:13:-1]
+print(qpo)
+
+# prints ebcba
+ebcba = letters[4::-1]
+print(ebcba)
+
+# prints the last 8 characters in order
+last8 = letters[-1:-9:-1]
+print(last8)
