fc03de19d020069c0bc372e0cc77f9d330edba58
T U <tylerletendre98@gmail.com>
2021-08-08 20:36:12 -0400

    finished refielding string operator

--- refields.py
+++ refields.py
@@ -1,2 +1,11 @@
 age = 24
-print("My age is " + str(age) + " years old")
+print("My age is {0} years old".format(age))
+
+# Uses the numbers and then formats it in the order its established
+print(
+    "There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, and {7} ".format(
+        31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"
+    )
+)
+
+print("Jan : {2}, Feb: {0}, Mar: {2}, Apr:{1}, May:{2}, Jun:{1}".format(28, 30, 31))
